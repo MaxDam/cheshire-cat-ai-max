cf37339ed48fad5873867eb464cb823615bd716f
D'Amico Massimiliano <Massimiliano.DAmico@exprivia.com>
2024-03-05 12:30:47 +0100

    created method classify() into stray_cat

--- core/cat/looking_glass/stray_cat.py
+++ core/cat/looking_glass/stray_cat.py
@@ -6,6 +6,8 @@ from typing import Literal, get_args
 from langchain.docstore.document import Document
 from langchain_community.llms import BaseLLM
 from langchain_core.language_models.chat_models import BaseChatModel
+from langchain.chains import LLMChain
+from langchain_core.prompts.prompt import PromptTemplate
 
 from fastapi import WebSocket
 
@@ -361,6 +363,53 @@ class StrayCat:
             content_type="text/plain"
         )
         self.rabbit_hole.store_documents(self, docs=docs, source="")
+
+    # Receives as input an action enum (ActionName and Description) and a user message, 
+    # and based on that chooses which element of the enum to return (the first element is by default)
+    def classify(self, actions, user_message):
+
+        # Get default action (first element)
+        default_action = next(iter(actions))
+
+        # Compose prompt
+        prompt = f"""
+    Your task is to produce a JSON representing which action the user chooses based on UserMessage. 
+    The possible actions are the following, in case of doubt answer with <{default_action.name}>:
+    """
+        for action in actions:
+            prompt += f"\n<{action.name}> {action.value}"
+            
+        prompt += f"""
+        
+    and return the corresponding tag for the action to be taken in the JSON format,
+    JSON must be in this format:
+    {{
+        "action": "<{default_action.name}>"
+    }}
+    User said "{user_message}"
+
+    JSON:
+    ```json
+    {{
+        "action":"""
+
+        # Escape curly braces in the prompt
+        prompt = prompt.replace("{", "{{").replace("}", "}}")
+
+        # Invoke LLM chain
+        extraction_chain = LLMChain(
+            prompt     = PromptTemplate.from_template(prompt),
+            llm        = self._llm,
+            verbose    = True,
+            output_key = "output"
+        )
+        response = extraction_chain.invoke({"stop": ["```"]})["output"]
+        
+        # Locates the returned action
+        for action in actions:
+            if "<" + action.name + ">" in response.upper():
+                return action
+        return default_action
 
     @property
     def user_id(self):
